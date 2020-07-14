80ce5c41d84950aac3c0f2f0c3c8a2cd97113a69
distalphalanges <ioan.a.mercea@gmail.com>
2020-07-14 14:10:13 +1000

    Override Card equality operator

--- lib/card.rb
+++ lib/card.rb
@@ -1,1 +1,15 @@
-Card = Struct.new(:question, :answer, :category)
+class Card
+  attr_reader :question, :answer, :category
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+
+  def ==(other)
+    self.question == other.question &&
+    self.answer == other.answer &&
+    self.category == other.category
+  end
+end
